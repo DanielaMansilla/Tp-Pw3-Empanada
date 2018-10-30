8d9d592860921e32b10c03c87cdcb26e90a4718c
Angela Mansilla <DaniMansilla94@outlook.com>
2018-10-30 02:25:32 -0300

    Se sube el proyecto

---
+++ W3_2018_2C_TP/W3_2018_2C_TP/Controllers/PedidosController.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace W3_2018_2C_TP.Controllers
+{
+    public class PedidosController : Controller
+    {
+        // GET: Pedidos
+        public ActionResult Iniciar()
+        {
+            return View();
+        }
+
+        public ActionResult Lista()
+        {
+            return View();
+        }
+
+        public ActionResult Iniciado()
+        {
+            return View();
+        }
+
+        public ActionResult Editar()
+        {
+            return View();
+        }
+
+        public ActionResult Eliminar()
+        {
+            return View();
+        }
+
+        public ActionResult Elegir()
+        {
+            return View();
+        }
+
+        public ActionResult Detalle()
+        {
+            return View();
+        }
+    }
+}
---
+++ W3_2018_2C_TP/W3_2018_2C_TP/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="W3_2018_2C_TP.MvcApplication" Language="C#" %>
